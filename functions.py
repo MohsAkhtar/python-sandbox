0bef42961cad35be2cfa7cbf175c90a75ed04aca
MohsAkhtar <mohsin.akhtar0@gmail.com>
2018-10-16 18:27:40 +0100

    Finished conditionals

--- functions.py
+++ functions.py
@@ -1,7 +1,46 @@
 # A function is a block of code which only runs when it is called. In Python, we do not use parentheses and curly brackets, we use indentation with tabs or spaces
 
 
+# Create function - Comments used to define what function does. Convention is it starts with capital and ends with period
+def sayHello():
+    """
+    Prints Hello and then name. 
+    """
+    print('Hello')
+
+# Create function with parameter
+def sayHello(name):
+    print('Hello ' + name)
+
+seyHello('Mohs')          # Call function
+
+# Create function with default parameter
+def sayHello(name='Beth'):
+    print('Hello ' + name)
+
+sayHello('Sam')           # Overwrites default parameter
+
+# Return value
+def getSum(num1, num2):
+    total = num1 + num2
+    return total
+
+print(getSum(4,5))        # 9
+
+
+def addOneToNum(num):
+    num += 1
+    return num
+
+num = 5
+new_num = addOneToNum(num)
+print(new_num)            # 6
 
 # A lambda function is a small anonymous function.
 # A lambda function can take any number of arguments, but can only have one expression. Very similar to JS arrow functions
 
+getSum = lambda num1, num2: num1 + num2     # do not need return statement
+print(getSum(9,2))         # 11
+
+addOneToNum = lambda num: num + 1
+print(addOneToNum(11))     # 12
--- tuples_sets.py
+++ tuples_sets.py
@@ -1,5 +1,43 @@
 # A Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
- 
+
+# Simple tuple
+fruit_tuple = ('Apple', 'Orange', 'Mango')
+
+# Create tuple using a constructor
+fruit_tuple = tuple(('Apple', 'Orange', 'Mango'))
+
+# Get a single value
+print(fruit_tuple[1])
+
+# You cannot change values of tuples
+fruit_tuple[1] = 'Grape'        # error
+
+# Tuples with one value should have trailing comma
+fruit_tuple_2 = ('Apple',)
+
+# Get length
+print(len(fruit_tuple))
+
+# Delete tuple
+del fruit_tuple
 
 
 # A Set is a collection which is unordered and unindexed. No duplicate members.
+
+# Create set
+fruit_set = {'Apple', 'Orange', 'Mango'}
+
+# Check if in set
+print('Apples' in fruit_set)
+
+# Add to set
+fruit_set.add('Grape')
+
+# Remove from set
+fruit_set.remove('Grape')
+
+# Clear set
+fruit_set.slear()
+
+# Delete set
+del fruit_set
