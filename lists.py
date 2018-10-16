1046bbe7575f4b89c45b26ca79023736f58a984c
MohsAkhtar <mohsin.akhtar0@gmail.com>
2018-10-16 17:32:03 +0100

    Finished variables, strings, and lists

--- lists.py
+++ lists.py
@@ -1,1 +1,37 @@
 # A List is a collection which is ordered and changeable. Allows duplicate members.
+# Basically an array
+
+# Create list
+numbers = [1,2,3,4,5]
+
+#Create using a constructor
+numbers = list((1,2,3,4,5))
+
+fruits = ['Apples', 'Oranges', 'Grapes', 'Pears']
+
+# Get value
+print(fruits[1])    # Oranges
+
+# Get length 
+print(len(fruits))  # 4
+
+# Append to list
+fruits.append('Mangos')
+
+# Remove from list
+fruits.remove('Grapes')
+
+# Insert into position
+fruits.insert(2, 'Strawberries')
+
+# Remove from certain position
+fruits.pop(3)
+
+# Reverse list
+fruits.reverse()
+
+# Sort list
+fruits.sort()
+
+# Reverse sort
+fruits.sort(reverse=True)
--- variables.py
+++ variables.py
@@ -13,3 +13,24 @@ VARIABLE RULES:
   - Must start with a letter or an underscore
   - Can have numbers but can not start with one
 """
+
+x = 1           # int
+y = 2.5         # float
+name = 'Mohs'   # string
+is_cool = True  # bool
+
+# Multiple assignment
+x, y, name, is_cool = (1, 2.5, 'Mohs', True)
+
+# Output something
+print(x)
+
+# Basic Math
+a = x + y
+print(a)          # 3.5
+
+# Check type
+print(type(x))    # <class 'int'>
+
+# Casting
+x = str(x)        # x is now string
